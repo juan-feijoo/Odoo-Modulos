f2bc0246e1aa1e8d6a08d46943b2edfea6535b0d
Juan Martin Feijoo <juan.feijoo@outsourcearg.com>
2025-09-01 12:13:02 -0300

    Mejorar la validación de stock en 'button_validate' para incluir la ubicación en la verificación de cantidades disponibles.

--- stock_unpack_validation/models/stock_picking.py
+++ stock_unpack_validation/models/stock_picking.py
@@ -8,30 +8,41 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def button_validate(self):
-        """ Valida que haya stock suficiente en el paquete al validar un traslado. """
+        """
+        Valida que haya stock suficiente en el paquete al validar un traslado.
+        """
+        # Usamos defaultdict para agrupar las demandas por paquete, producto y ubicación
         demanded_quantities = defaultdict(float)
 
         for line in self.move_line_ids:
+
             if line.qty_done > 0 and line.package_id and line.location_id.usage == 'internal':
-                key = (line.package_id, line.product_id)
+                key = (line.package_id, line.product_id, line.location_id)
                 demanded_quantities[key] += line.qty_done
 
         if demanded_quantities:
-            for (package, product), demanded_qty in demanded_quantities.items():
-                available_qty = self.env['stock.quant']._get_available_quantity(
-                    product, package.location_id, package_id=package, strict=True
-                )
+            for (package, product, location), demanded_qty in demanded_quantities.items():
+                
+
+                quants = self.env['stock.quant'].search([
+                    ('package_id', '=', package.id),
+                    ('product_id', '=', product.id),
+                    ('location_id', '=', location.id),
+                ])
+                available_qty = sum(quants.mapped('quantity'))
 
                 if float_compare(demanded_qty, available_qty, precision_rounding=product.uom_id.rounding) > 0:
                     raise ValidationError(
-                        _("Validación Fallida: No hay suficiente stock para el producto '%(product_name)s' en el paquete '%(package_name)s'.\n\n"
+                        _("Validación Fallida: No hay suficiente stock para el producto '%(product_name)s' en el paquete '%(package_name)s' desde la ubicación '%(location_name)s'.\n\n"
                           "Cantidad Requerida: %(demanded_qty)s\n"
                           "Cantidad Disponible: %(available_qty)s") % {
                               'product_name': product.display_name,
                               'package_name': package.name,
+                              'location_name': location.display_name,
                               'demanded_qty': demanded_qty,
                               'available_qty': available_qty,
                           }
                     )
         
+        # Si todas las validaciones pasan, se ejecuta la lógica original de Odoo.
         return super(StockPicking, self).button_validate()
