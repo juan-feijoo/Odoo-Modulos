6e06da7423bfaf6ca2d0744b8aa9ea67c918ba30
Juan Martin Feijoo <juan.feijoo@outsourcearg.com>
2025-09-25 12:30:56 -0300

    Actualizar la categoría del módulo 'Fix: Recálculo de Moneda en Asientos Contables' a 'Accounting/Accounting' y eliminar propiedades innecesarias en el manifiesto.

--- invoice_recalculation_fix/__manifest__.py
+++ invoice_recalculation_fix/__manifest__.py
@@ -2,15 +2,12 @@
 {
     "name": "Fix: Recálculo de Moneda en Asientos Contables",
     "version": "17.0.1.0.0",
-    "category": "Tools",
+    "category": "Accounting/Accounting",
     "summary": "Fuerza la regeneración de apuntes contables en facturas con moneda extranjera para usar la tasa de cambio correcta.",
     "author": 'Juan Feijoo',
     "depends": ['account',],
     "data": [
     ],
-    "assets": {},
     "license": "LGPL-3",
     "installable": True,
-    "auto_install": False,
-    "application": True
 }
