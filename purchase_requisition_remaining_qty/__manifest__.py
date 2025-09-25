1cf81a6b7845b6b5ac8b066699797b35613bf5a8
Juan Martin Feijoo <juan.feijoo@outsourcearg.com>
2025-09-25 11:51:22 -0300

    Actualizar el resumen del módulo y agregar la vista para ocultar botones en estado cancelado en la Orden de Compra.

--- purchase_requisition_remaining_qty/__manifest__.py
+++ purchase_requisition_remaining_qty/__manifest__.py
@@ -3,15 +3,19 @@
     "name": "Purchase Requisition Remaining Qty",
     "version": "17.0.1.0.0",
     "category": "Tools",
-    "summary": "Auto-generated module",
+    "summary": "Validacion de Requerimiento de compras",
     'description': """
         Este módulo mejora la funcionalidad estándar del Acuerdo de Compra.
         Cuando un usuario crea una Orden de Compra a partir de una línea del acuerdo, 
         el sistema calculará automáticamente (Cantidad Total - Cantidad Solicitada) y propondrá este importe restante,
         en lugar de la cantidad total.
     """,
-    "depends": ["purchase_requisition"],
-    "data": [],
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        'views/purchase_order_view.xml',
+    ],
     "assets": {},
     "license": "LGPL-3",
     "installable": True,
