9b9bcfebb9a09774466e99d5ba4308e7bfb3fd7c
Juan Martin Feijoo <juan.feijoo@outsourcearg.com>
2025-10-23 14:45:10 -0300

    Agregar desglose de bases imponibles de IVA y percepciones de Ingresos Brutos en el informe fiscal

--- custom_ar_reports/models/account_tax_report_handler.py
+++ custom_ar_reports/models/account_tax_report_handler.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, _
 from odoo.exceptions import UserError
 
 class ArgentinianReportCustomHandler(models.AbstractModel):
@@ -7,43 +7,82 @@ class ArgentinianReportCustomHandler(models.AbstractModel):
 
     def _build_query(self, report, options, column_group_key):
         """
-        Heredamos la función que construye el query de SQL para añadir nuestras
-        columnas de base imponible directamente en la consulta.
+        Versión final que incluye el desglose de bases imponibles de IVA y el
+        desglose de las percepciones de Ingresos Brutos por jurisdicción.
         """
         query, params = super()._build_query(report, options, column_group_key)
 
-        TaxGroup = self.env['account.tax.group']
-        # Buscamos los grupos de impuestos por su código AFIP
-        tax_group_10_5 = TaxGroup.search([('l10n_ar_vat_afip_code', '=', '4')], limit=1)
-        tax_group_21 = TaxGroup.search([('l10n_ar_vat_afip_code', '=', '5')], limit=1)
+        # Lógica de valor condicional que asegura el signo correcto en Ventas y Compras.
+        value_logic = """
+            (CASE 
+                WHEN COALESCE(nt.type_tax_use, bt.type_tax_use) = 'sale'
+                THEN -("account_move_line".balance) 
+                ELSE "account_move_line".balance 
+            END)
+        """
 
-        if not tax_group_10_5 or not tax_group_21:
-            raise UserError("No se pudieron encontrar los grupos de impuestos para IVA 10.5% (código AFIP 4) o 21% (código AFIP 5). Verifique la configuración de la localización argentina.")
-
+        # Añadimos los campos para las bases de IVA y las nuevas percepciones.
         new_fields = f"""
-            , SUM(CASE WHEN "account_move_line".tax_group_id = {tax_group_10_5.id} THEN "account_move_line".tax_base_amount ELSE 0 END) AS gravado_10_5
-            , SUM(CASE WHEN "account_move_line".tax_group_id = {tax_group_21.id} THEN "account_move_line".tax_base_amount ELSE 0 END) AS gravado_21
+            , SUM(CASE 
+                WHEN btg.l10n_ar_vat_afip_code = '4' 
+                THEN {value_logic} ELSE 0 
+            END) AS gravado_10_5
+            , SUM(CASE 
+                WHEN btg.l10n_ar_vat_afip_code = '5' 
+                THEN {value_logic} ELSE 0 
+            END) AS gravado_21
+            , SUM(CASE 
+                WHEN btg.l10n_ar_vat_afip_code = '6' 
+                THEN {value_logic} ELSE 0 
+            END) AS gravado_27
+            
+            , SUM(CASE 
+                WHEN ntg.l10n_ar_vat_afip_code IS NULL AND ntg.l10n_ar_tribute_afip_code = '07' AND ntg.name->>'es_AR' ILIKE '%%Perc IIBB Catamarca%%'
+                THEN {value_logic} ELSE 0 
+            END) AS perc_iibb_catamarca
+            
+            , SUM(CASE 
+                WHEN ntg.l10n_ar_vat_afip_code IS NULL AND ntg.l10n_ar_tribute_afip_code = '07' AND ntg.name->>'es_AR' ILIKE '%%Perc IIBB Santiago del Estero%%'
+                THEN {value_logic} ELSE 0 
+            END) AS perc_iibb_sgo
+
+            , SUM(CASE 
+                WHEN ntg.l10n_ar_vat_afip_code IS NULL AND ntg.l10n_ar_tribute_afip_code = '07' AND ntg.name->>'es_AR' ILIKE '%%Perc IIBB Neuquén%%'
+                THEN {value_logic} ELSE 0 
+            END) AS perc_iibb_neuquen
+            
+            , SUM(CASE 
+                WHEN ntg.l10n_ar_vat_afip_code IS NULL AND ntg.l10n_ar_tribute_afip_code = '07' AND ntg.name->>'es_AR' ILIKE '%%Perc IIBB OTROS%%'
+                THEN {value_logic} ELSE 0 
+            END) AS perc_iibb_otros
+            
         """
        
-        if 'SUM(account_move_line.balance) AS total' in query:
+        anchor = 'SUM(account_move_line.balance) AS total'
+        if anchor in query:
              query = query.replace(
-                'SUM(account_move_line.balance) AS total',
-                'SUM(account_move_line.balance) AS total' + new_fields
+                anchor,
+                anchor + new_fields
             )
         else:
-            select_str = "SELECT %s AS column_group_key,"
-            query = query.replace(select_str, select_str + new_fields.replace(',', '', 1))
+            raise UserError(_("No se pudo modificar la consulta SQL del reporte de IVA. El punto de anclaje no fue encontrado."))
 
         return query, params
 
     def _dynamic_lines_generator(self, report, options, all_column_groups_expression_totals, warnings=None):
-
+        # Aquí registramos los nombres de nuestros nuevos campos para que Odoo los trate como números.
         original_number_keys = list(self.number_keys)
         try:
-            if 'gravado_10_5' not in self.number_keys:
-                self.number_keys.append('gravado_10_5')
-            if 'gravado_21' not in self.number_keys:
-                self.number_keys.append('gravado_21')
+            # Campos de Gravado
+            if 'gravado_10_5' not in self.number_keys: self.number_keys.append('gravado_10_5')
+            if 'gravado_21' not in self.number_keys: self.number_keys.append('gravado_21')
+            if 'gravado_27' not in self.number_keys: self.number_keys.append('gravado_27')
+            
+            # Campos de Percepciones IIBB
+            if 'perc_iibb_catamarca' not in self.number_keys: self.number_keys.append('perc_iibb_catamarca')
+            if 'perc_iibb_sgo' not in self.number_keys: self.number_keys.append('perc_iibb_sgo')
+            if 'perc_iibb_neuquen' not in self.number_keys: self.number_keys.append('perc_iibb_neuquen')
+            if 'perc_iibb_otros' not in self.number_keys: self.number_keys.append('perc_iibb_otros')
 
             lines = super()._dynamic_lines_generator(report, options, all_column_groups_expression_totals, warnings=warnings)
         finally:
